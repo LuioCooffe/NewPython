be71b5875b45eeae496e0f3a934a1ed94f940da0
LuioCooffe <128321969+LuioCooffe@users.noreply.github.com>
2023-03-19 16:58:28 -0300

    first facilitation

---
+++ NewPythonVER1.py
@@ -0,0 +1,15 @@
+add = append
+def HelloWorld:
+  print("Hello World")
+x = list()
+y = list()
+text = print
+put_text = input
+ten = 10
+ten_one = 11
+ten_two = 12
+ten_three = 13
+twenty = 20
+twenty_one = 21
+twenty_two = 22
+twenty_three = 23
